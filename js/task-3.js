bef2d1aebd6e148eb4c4b99a5c41b843c917a037
samuelzj <k.habek@yahoo.com>
2024-04-20 10:24:49 +0200

    ready

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 function getElementWidth(content, padding, border) {
-    return
+    return (
     Number.parseFloat(content) +
     2 * Number.parseFloat(padding) +
-    2 * Number.parseFloat(border);
+    2 * Number.parseFloat(border));
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
